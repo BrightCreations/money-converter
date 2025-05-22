67dc6238e31b92d60a478fa551a360b1ed58625c
kareemshaaban-bc <kareem.shaaban@brightcreations.com>
2025-05-22 23:03:34 +0300

    Add constants to facade class

--- src/Facades/MoneyConverter.php
+++ src/Facades/MoneyConverter.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Facade;
 
 class MoneyConverter extends Facade
 {
+    public const ON_FAIL_THROW_EXCEPTION = 1;
+    public const ON_FAIL_FETCH_EXCHANGE_RATES = 2;
+
     protected static function getFacadeAccessor(): string
     {
         return MoneyConverterInterface::class;
